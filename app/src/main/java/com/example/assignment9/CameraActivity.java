4698f2cba710e157ef820a741f6707e5a3a59fd8
Brilord <brilord1000@gmail.com>
2022-11-08 05:50:42 -0500

    working progress

--- app/src/main/java/com/example/assignment9/CameraActivity.java
+++ app/src/main/java/com/example/assignment9/CameraActivity.java
@@ -1,15 +1,47 @@
 package com.example.assignment9;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.common.util.concurrent.ListenableFuture;
 
 public class CameraActivity extends AppCompatActivity {
-
+    boolean defCamera;
+    PreviewView previewView;
+    Preview preview;
+    Button capture, switchCamera;
+    CameraSelector cameraSelector;
+    ProcessCameraProvider cameraProvider;
+    private final int REQUEST_CODE_PERMISSIONS = 100;
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA","android.permission.WRITE_EXTERNAL_STORAGE"};
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private ImageCapture imageCapture;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        defCamera = true;
 
+        previewView = findViewById(R.id.previewView);
+        capture = findViewById(R.id.camera_btn);
+        switchCamera = findViewById(R.id.switch_camera_btn);
+
+        capture.setOnClickListener(this);
+        switchCamera.setOnClickListener(this);
+
+        if (allPermissionsGranted()) {
+            startCameraX();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
     }
 }
