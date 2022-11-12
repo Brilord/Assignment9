b14ca2c1f0a71b9e80afecd58c782b1f0d441b45
Brilord <95247252+Brilord@users.noreply.github.com>
2022-11-11 20:00:06 -0500

    working progress

---
+++ app/src/main/java/com/example/assignment9/ImageActivity.java
@@ -0,0 +1,33 @@
+package com.example.assignment9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firestore.v1.FirestoreGrpc;
+
+public class ImageActivity extends AppCompatActivity {
+
+    ImageView imageView2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+
+        imageView2 = findViewById(R.id.imageView22);
+
+        StorageReference storageRef;
+        FirebaseStorage storage;
+        StorageReference mountainRef;
+
+        storage = FirebaseStorage.getInstance();
+
+        storageRef = storage.getReference();
+        mountainRef = storageRef.child("Image").child();
+        Glide.with(this).load(mountainRef).into(imageView2);
+    }
+}
---
+++ app/src/main/java/com/example/assignment9/NotesAdapter.java
@@ -0,0 +1,22 @@
+package com.example.assignment9;
+
+import android.content.Context;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class NotesAdapter extends RecyclerView.Adopter<NotesAdapter.MyHolder> {
+    public NotesAdapter(List<Note> noteslist, Context context) {
+        this.context = context;
+        n
+    }
+
+
+
+
+
+    public class MyHolder extends RecyclerView.ViewHolder {
+
+    }
+}
