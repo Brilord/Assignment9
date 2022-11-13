d40be1e2e3bafe10a562c63280ceec2918773941
Brilord <brilord1000@gmail.com>
2022-11-13 18:30:47 -0500

    working progress

--- app/src/main/java/com/example/assignment9/MainActivity.java
+++ app/src/main/java/com/example/assignment9/MainActivity.java
@@ -127,4 +127,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         }
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // register this class as a listener for the orientation and
+        // accelerometer sensors
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    @Override
+    protected void onPause() {
+        // unregister listener
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
 }
